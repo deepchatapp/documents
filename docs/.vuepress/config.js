f93b77cb8b264bf0c249ac8e2d695a34b0f8446d
袁鑫 <eric.yuanxin@gmail.com>
2025-03-23 21:36:18 +0800

    modify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,41 +8,42 @@ module.exports = {
   themeConfig: {
     logo: 'https://deepchat.thinkinai.xyz/logo.png',
     nav: [
-      { text: '首页', link: '/' },
-      { text: '快速开始', link: '/guide/getting-started/' },
-      { text: '使用指南', link: '/guide/user-guide/' },
       { text: '官网', link: 'https://deepchat.thinkinai.xyz/' }
     ],
-    sidebar: {
-      '/guide/getting-started/': [
-        {
-          title: '快速开始',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            '',
-            'installation',
-            'initial-setup',
-            'first-conversation',
-          ]
-        }
-      ],
-      '/guide/user-guide/': [
-        {
-          title: '使用指南',
-          collapsable: false,
-          sidebarDepth: 2,
-          children: [
-            '',
-            'model-configuration',
-            'document-processing',
-            'artifacts',
-            'mcp',
-            'knowledge-base',
-          ]
-        }
-      ]
-    },
+    sidebar: [
+      {
+        title: 'DeepChat介绍',
+        path: '/',
+        collapsable: true,
+        sidebarDepth: 0
+      },
+      {
+        title: '快速开始',
+        path: '/guide/getting-started/',
+        collapsable: true,
+        sidebarDepth: 2,
+        children: [
+          '/guide/getting-started/',
+          '/guide/getting-started/installation',
+          '/guide/getting-started/initial-setup',
+          '/guide/getting-started/first-conversation',
+        ]
+      },
+      {
+        title: '使用指南',
+        path: '/guide/user-guide/',
+        collapsable: true,
+        sidebarDepth: 2,
+        children: [
+          '/guide/user-guide/',
+          '/guide/user-guide/model-configuration',
+          '/guide/user-guide/document-processing',
+          '/guide/user-guide/artifacts',
+          '/guide/user-guide/mcp',
+          '/guide/user-guide/knowledge-base',
+        ]
+      }
+    ],
     // 平滑滚动
     smoothScroll: true,
     // 最后更新时间
@@ -50,6 +51,9 @@ module.exports = {
     // 显示编辑链接
     editLinks: false,
     // 默认展开所有侧边栏组
-    displayAllHeaders: false
+    displayAllHeaders: false,
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10
   }
 } 
